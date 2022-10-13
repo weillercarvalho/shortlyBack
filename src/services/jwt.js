6790f53e7baf7fbdc6ce4152dab540f867fb39d7
weillercarvalho <engweiller@hotmail.com>
2022-10-13 19:22:30 -0300

    feat [].

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = (email) => jwt.sign({email}, process.env.SECRET_KEY, {expiresIn: 864000})
+const generateToken = (email) => jwt.sign({email}, process.env.TOKEN_SECRET, {expiresIn: 864000})
 
 
 export {generateToken};
